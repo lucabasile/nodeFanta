494197b2ab1e0eb3b7fb980c17378dbceced9a0a
Basile <luca.basile@capgemini.com>
2022-02-14 19:22:02 +0100

    go to Heroku

--- app.js
+++ app.js
@@ -3,44 +3,45 @@ const http = require('http');
 /*this function can have any name, it just need a couple of arguments
 (you can give them any name but the first is always the request and rhe second the response)
 */
-const reqListener = function (req,res){
+const reqListener = function (request,response){
     /*res.writeHead(200);
     res.end('Hello World Again!');*/
 
-    const url= req.url;
-    const method= req.method;
+    const url= request.url;
+    const method= request.method;
 
     if(url === '/insert'){
-        res.write('<html>');
-        res.write('<head><title>A Simple form</title></head>');
-        res.write('<body>');
-        res.write('<h1 style="color:blue;background-color:orange;text-align:center">Enter Data</h1>');
-        res.write('<div><form action="/message" method="POST"><input type="text" name="message"/><button>Send</button></form></div>');
-        res.write('</body>');
-        res.write('</html>');
+        response.write('<html>');
+        response.write('<head><title>A Simple form</title></head>');
+        response.write('<body>');
+        response.write('<h1 style="color:blue;background-color:orange;text-align:center">Enter Data</h1>');
+        response.write('<div><form action="/message" method="POST"><input type="text" name="message"/><button>Send</button></form></div>');
+        response.write('</body>');
+        response.write('</html>');
         //If I don't use the return the code breaks...WHY?
-        return res.end();
+        return response.end();
     }
 
     if(url === '/message' && method === 'POST'){
-        console.log("You\'ve sent a message with a POST");
-        res.statusCode= 302; 
-        res.setHeader('Location','/');
+        
+        response.statusCode= 302; 
+        response.setHeader('Location','/insert');
 
-        //req.on('data')
+        request.on('data',()=>{console.log('I\'ve finished receiving your message')});
+        console.log("You\'ve sent a message with a POST");
         //If I don't use the return the code breaks...WHY?
-        return res.end();
+        return response.end();
     }
 
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>A Simple page</title></head>');
-    res.write('<body>');
-    res.write('<h1 style="color:blue;background-color:cyan;text-align:center">Hello From Node</h1>');
-    res.write('<div>This is a static page</div>');
-    res.write('</body>');
-    res.write('</html>');
-    return res.end();
+    response.setHeader('Content-Type','text/html');
+    response.write('<html>');
+    response.write('<head><title>A Simple page</title></head>');
+    response.write('<body>');
+    response.write('<h1 style="color:blue;background-color:cyan;text-align:center">Hello From Node</h1>');
+    response.write('<div>This is a static page</div>');
+    response.write('</body>');
+    response.write('</html>');
+    return response.end();
 
 
 }
