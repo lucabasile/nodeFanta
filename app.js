61f6c0c5cf03c41ac72488d4f2b4f68473c7348a
Basile <luca.basile@capgemini.com>
2022-02-16 18:45:11 +0100

    added body-parser

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
 const funzica= require('./routes');
 const express= require('express');
+const bodyParser = require('body-parser');
 
 const app=express();
+app.use( bodyParser.json() );//Serve per parsare i body in formato json
+app.use(bodyParser.urlencoded({ extended: true }));//Serve per parsare i body url-encoded
 
 const port = process.env.PORT || 3000
 
@@ -10,10 +13,10 @@ app.get('/ciao',(req,res)=>{
     return res.end('Ciao a tutti');
 });
 
-/*app.post('/postalo',(req,res)=>{
+app.post('/postalo',(req,res)=>{
     console.log(req.body);
-    return;
-});*/
+    return res.sendStatus(200);
+});
 
 const server = http.createServer(app);
 
